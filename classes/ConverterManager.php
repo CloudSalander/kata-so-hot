5da699570530206da9a9511d15eecf325c503872
CloudSalander <ruben.as532@gmail.com>
2025-07-31 09:52:28 +0200

    Reading and validating degrees

--- classes/ConverterManager.php
+++ classes/ConverterManager.php
@@ -5,5 +5,24 @@ include('FarenheitConverter.php');
 include('CelsiusConverter.php');
 
 class ConverterManager {
+    
+    //TODO: Maybe all info in one input?
+    const ASK_DEGREES = "Quants graus fan?";
+    const ASK_SCALE = "Escala?(C,F,K)";
+    
+    private float $degrees;
+    private string $scale;
   
+    public function ask() {
+        $degrees = $this->readDegrees();
+        $scale = $this->readScale();
+    }
+
+    private function readDegrees(): float {
+       do{
+            $degrees = readline(self::ASK_DEGREES);
+       }while(filter_var($degrees, FILTER_VALIDATE_FLOAT) === false);
+       
+       return floatval($degrees);
+    }
 }
--- index.php
+++ index.php
@@ -3,3 +3,4 @@
 include('classes/ConverterManager.php');
 
 $converterManager = new ConverterManager();
+$converterManager->ask();
